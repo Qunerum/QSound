2d380cb97a0aad398f680541386212bc76b2ffaf
Qunerum <qunerumofficial@gmail.com>
2026-08-13 21:08:30 +0200

    update 2026-08-13 21:08

--- src/main.c
+++ src/main.c
@@ -4,13 +4,13 @@
 
 int main(void) {
 	if (!qsInit()) return 1;
-	qsConvert("franxx.qsr", "franxx.qs");
+	qsConvert("soundsReadable/music.qsr", "sounds/music.qs");
 
-	int sndFile = qsOpen("franxx.qs");
+	int sndFile = qsOpen("sounds/music.qs");
 
 	printf("'music.qs'...\n");
 	qsPlay(sndFile, 100, 0.6f);
-	usleep(100000000);
+	usleep(13000000);
 
 	qsClose();
 	return 0;
