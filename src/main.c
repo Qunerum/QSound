cb1d57f302cd3e82cc9c9f53cd481d184f974cfc
Qunerum <qunerumofficial@gmail.com>
2026-08-14 19:05:50 +0200

    update 2026-08-14 19:05

--- src/main.c
+++ src/main.c
@@ -12,7 +12,8 @@ int main(void) {
 
 	qsConvert("soundsReadable/music.qsr", "sounds/music.qs");
 
-	int sndSine = qsOpen("sounds/sine.qs"),
+	int
+	sndSine = qsOpen("sounds/sine.qs"),
 	sndSquare = qsOpen("sounds/square.qs"),
 	sndSawtooth = qsOpen("sounds/sawtooth.qs"),
 	sndTriangle = qsOpen("sounds/triangle.qs"),
@@ -20,22 +21,22 @@ int main(void) {
 	sndFile = qsOpen("sounds/music.qs");
 
 	printf("'sine.qs'...\n");
-	qsPlay(sndSine, 100, 1);
+	qsPlay(sndSine, 255, 1);
 	usleep(1500000);
 	printf("'square.qs'...\n");
-	qsPlay(sndSquare, 100, 1);
+	qsPlay(sndSquare, 255, 1);
 	usleep(1500000);
 	printf("'sawtooth.qs'...\n");
-	qsPlay(sndSawtooth, 100, 1);
+	qsPlay(sndSawtooth, 255, 1);
 	usleep(1500000);
 	printf("'triangle.qs'...\n");
-	qsPlay(sndTriangle, 100, 1);
+	qsPlay(sndTriangle, 255, 1);
 	usleep(1500000);
 	printf("'noise.qs'...\n");
-	qsPlay(sndNoise, 100, 1);
+	qsPlay(sndNoise, 255, 1);
 	usleep(1500000);
 	printf("'music.qs'...\n");
-	qsPlay(sndFile, 100, 0.75f);
+	qsPlay(sndFile, 255, 0.75f);
 	usleep(13000000);
 
 	qsClose();
